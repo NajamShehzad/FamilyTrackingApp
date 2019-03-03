d19aff0488286541001d42f312bec72a33fb7d9e
muneebwasikhan <muneebwasikhan@gmail.com>
2019-03-03 13:56:38 +0500

    socket.io live event

--- NodeExpressBoilerplate/routes/location.js
+++ NodeExpressBoilerplate/routes/location.js
@@ -3,18 +3,34 @@ exports = module.exports = function (app, mongoose) {
     var express = require('express');
     var router = express.Router();
 
-    router.post('/updatelocation', async function (req, res, next) {
+    router.post('/update', async function (req, res, next) {
         try {
             let CircleModel = app.db.models.Circle;
-            let newobj = await CircleModel.updateMany(
-                { "circleMembers.memberId": req.body.memberId },
-                { $set: { "circleMembers.$.latitude": "954786321", "circleMembers.$.longitude": "754786321" } },
+            let newobj = await CircleModel.findOneAndUpdate(
+                { _id: req.body.circleId, "circleMembers.memberId": req.body.memberId },
+                { $set: { "circleMembers.$.latitude": "254786321", "circleMembers.$.longitude": "254786321" } },
                 { new: true }
             );
-            console.log(newobj)
+            // console.log(newobj)
+
+            res.send({
+                success: true,
+                message: "Location Update Succesfully"
+            });
+            app.io.emit(req.body.circleId,
+                {
+                    memberId: req.body.memberId,
+                    latitude: "121212",
+                    longitude: "12547854"
+                }
+            );
 
         } catch (err) {
             console.log(err);
+            res.send({
+                success: false,
+                message: err.message
+            })
         }
     });
 
