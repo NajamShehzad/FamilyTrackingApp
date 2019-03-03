e22e558f4302c9edf73fb2348dfbe5fb61ceff9f
NajamShehzad <najambutt195@gmail.com>
2019-03-03 07:38:25 +0500

    sendGridAPI

--- NodeExpressBoilerplate/config/index.js
+++ NodeExpressBoilerplate/config/index.js
@@ -1,7 +1,10 @@
 exports = module.exports = function (app) {
     // app.set("mongodb-url", "mongodb://localhost:27017/SocketIo");
     app.set("mongodb-url", "mongodb://najam:najam123@ds135179.mlab.com:35179/reactnative");
+    app.set("passSalt", "somemagicalwords")
     app.set("cloud_name", "muneebwasikhan");
     app.set("api_key", "731288742293688");
     app.set("api_secret", "nGImMvE81Yetl2GNWjpTMOr11kM");
+
+    app.set('sendgrid_api_key', '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');
 }
